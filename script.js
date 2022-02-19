4574e7940d5acfcb77ab95cc6e4d410bbf090298
ahmed-razic <ahmed.razic@gmail.com>
2022-02-19 17:06:31 +0100

    finished

--- script.js
+++ script.js
@@ -7,16 +7,50 @@
  * 6. Adjust UI states accordingly
  * 7. Bonus: change button CTA to indicate if it's the first joke or a "next" one
  */
-const API_ENDPOINT = 'https://icanhazdadjoke.xcom/';
+const API_ENDPOINT = 'https://icanhazdadjoke.com/';
+const buttonSelector = document.querySelector('#button');
+const messageSelector = document.querySelector('#error-message');
+const containerSelector = document.querySelector('#error-container');
+const jokeSelector = document.querySelector('#joke');
+const loaderSelector = document.querySelector('#loader');
+const ctaSelector = document.querySelector('#cta');
 
 const reqJoke = new XMLHttpRequest();
 
+function setUIState(isDisabled) {
+    setLoading(isDisabled);
+    setDisabled(isDisabled);
+}
+
 function showJoke(joke) {
-    document.querySelector('#joke').innerHTML = joke;
+    jokeSelector.innerHTML = joke;
+    setUIState(false);
 }
 function showError(error) {
-    document.querySelector('#error-message').innerHTML = error;
-    document.querySelector('#error-container').style.display = 'block';
+    messageSelector.innerHTML = error;
+    containerSelector.style.display = 'block';
+    setUIState(false);
+}
+
+function setLoading(isVisible) {
+    const displayState = isVisible ? 'block' : 'none';
+    loaderSelector.style.display = displayState;
+}
+
+function setDisabled(isDisabled) {
+    if (isDisabled) {
+        buttonSelector.setAttribute('disabled', 'disabled');
+    } else {
+        buttonSelector.removeAttribute('disabled');
+    }
+
+    const buttonCtaState = isDisabled ? 'none' : 'block';
+    ctaSelector.style.display = buttonCtaState;
+}
+
+function setCtaButton(isError) {
+    const buttonText = isError ? 'Try again' : 'Next Joke';
+    ctaSelector.innerHTML = buttonText;
 }
 
 function getJoke() {
@@ -26,14 +60,17 @@ function getJoke() {
 
     reqJoke.onload = function () {
         showJoke(reqJoke.response.joke);
+        setCtaButton(false);
     };
     reqJoke.onerror = function () {
         showError('Unknown error');
+        setCtaButton(true);
     };
 
     reqJoke.send();
 }
 
-document.querySelector('#button').addEventListener('click', function () {
+buttonSelector.addEventListener('click', function () {
+    setUIState(true);
     getJoke();
 });
